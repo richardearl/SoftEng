f9470f4939bd3f3ac0841b4b0cb52590ef35599c
rasee94 <keke.93@hotmail.co.uk>
2014-03-19 11:27:04 +0000

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -1,4 +1,21 @@
-
 public class Notes {
 
+    private String name;
+    private String body;
+    
+    public void setName(String name){
+      
+    }
+    
+    public String getName(){
+      
+    } 
+    
+    public void setBody(String body){
+      
+    }
+    
+    public String getBody(){
+      
+    }
 }
