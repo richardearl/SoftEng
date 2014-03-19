939054b4147ef8b4c83dfddcc4dc899daf4540a1
MarkJohnson94 <markwjohnson94@gmail.com>
2014-03-19 11:41:11 +0000

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -1,4 +1,39 @@
-
-public class Contact {
-
+class Contacts
+{
+	private String name;
+	private int phoneNo;
+	private String email;
+	private String dateOfBirth;
+	private boolean DoBHidden;
+	
+	public Contacts()
+	{
+		
+	}
+	
+	public void setName(String aName)
+	{
+		if (!aName.isEmpty())
+		{
+			name = aName;
+		}
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setPhoneNo(int aPhoneNo)
+	{
+		//if (aPhoneNo.length)
+		
+		phoneNo = aPhoneNo;
+	
+	}
+	
+	public int getPhoneNo()
+	{
+		return phoneNo;
+	}
 }
