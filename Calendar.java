497881387fabbcf843744fee2d73d09b1241d03a
Rich <richards.earl@ntlworld.com>
2014-03-19 11:30:09 +0000

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,4 +1,14 @@
-
 public class Calendar {
-
+  //Calendar should be a list of all days that can have multiple instances of appointment added to each day
+  public Calendar(){
+    int january = 31;
+    int february = 28;
+    int march = 31;
+    int april = 30;
+    int may = 31;
+    int june = 30;
+    int july = 31;
+    int august = 31;
+    
+  }
 }
