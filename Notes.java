aa85cef45d74feb22ccbbbba3e7576d991f2f7e9
BongoJongo <Bennybo100@yahoo.co.uk>
2014-03-26 11:54:48 +0000

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -1,30 +1,81 @@
+import java.util.Scanner;
+
 public class Notes {
 
-    private String name;
+    private String title;
     private String body;
+    private Notes note;
+    private String userInstruction;
+    private Scanner in;
+    private boolean noteTitlePresent;
+    private boolean noteBodyPresent;
     
-    public void addNote() {
+    public Notes() {
+    	
+    }
+    
+    //main is for testing purposes
+    
+    public static void main(String[] args) {
+    	Notes note = new Notes();
+    	note.Notes();
+            }
+    
+    public void Notes() {
+    	 Scanner in = new Scanner(System.in);
+    	 while (true) {
+             
+             if (in.nextLine().equals("add note")) {
+            	 System.out.println("OK");
+            	 title = in.nextLine();
+            	 System.out.println(title);
+                 addNote(title);
+             }
+             else if (in.nextLine().equals("Done")) {
+                 saveChanges();
+             }
+             
+         }
+    }
+    public void addNote(String title) {
         //Upon note creation, call setNoteTitle() and setNoteBody().
-        noteTitlePresent = setNoteTitle();
-        noteBodyPresent = setNoteBody();
+        noteTitlePresent = setNoteTitle(title);
+        noteBodyPresent = setNoteBody(body);
         
     }
     
-    public boolean setNoteTitle() {
-        
+    public boolean setNoteTitle(String title) {
+    	if (title != null) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
     }
     
-    public boolean setNoteBody(String body){
-      
+    public boolean setNoteBody(String body) {
+        if (body != null) {
+        	return true;
+        }
+        else {
+        	return false;
+        }
     }
     
     public void saveChanges() {
-        noteFinished = checkNoteComplete();
+        boolean noteFinished = checkNoteComplete();
+        if (noteFinished) {
+        	System.out.println(title+body);
+            //create the new note. For the GUI this will need to create a new button.
+        }
         
     }
     
     public boolean checkNoteComplete() {
+    	System.out.println(noteTitlePresent);
+    	System.out.println(noteBodyPresent);
         if ((noteTitlePresent && noteBodyPresent)) {
+        	System.out.println("oook");
             return true;
         }
         else { 
@@ -35,13 +86,13 @@ public class Notes {
       
     }
     
-    public String getName(){
+    public void getName(){
       
     } 
     
    
     
-    public String getBody(){
+    public void getBody(){
       
     }
 }
