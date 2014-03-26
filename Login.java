61b675a7fbdb0888f40e11e78a5c2d85ce512ed7
MarkJohnson94 <markwjohnson94@gmail.com>
2014-03-26 10:31:37 +0000

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,3 +1,43 @@
 public class Login {
 
+  private boolean userLogin =  false;    
+  private String userID = "";
+
+	public Login(boolean userLogin, String userID){
+		setUserLogin(userLogin);
+		setUserID(userID);
+	}
+	
+	public void resetVariable() {
+		setUserLogin(false);
+		setUserID("");
+		setUserType("");
+	}
+	
+  public void setUserLogin(boolean userLogin) {
+      this.userLogin = userLogin;
+  }
+  
+  public boolean isUserLogin() {
+      return userLogin;
+  }
+  
+  public void setUserID(String userID) {
+      this.userID = userID;
+  }
+  
+  public String getUserID() {
+      return userID;
+  }
+  
+  public void setUserType(String userType) {
+      this.userType = userType;
+  }
+  
+  public String getUserType () {
+      return userType;
+  }     
+    
+}
+
 }
