6ae7f38f8de75937dbc469c3ffc84b20b28dabff
Rich <richards.earl@ntlworld.com>
2014-04-01 11:43:50 +0100

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -25,13 +25,33 @@ public class Calendar {
       }
     }
   }
+  public void printAppointment(){
+    //prints all appointments
+    int x = 0;
+    while(x<366){
+    for(Appointment a: days[day]){
+      
+        System.out.println(a.getName());
+      
+    }
+    x++;
+  }
   
   public void userInput(){
     Scanner scan = new Scanner();
     System.out.println("Press 1 to add an Appointment");
     System.out.println("Press 2 to add an Remove");
     System.out.println("Press 3 to view the calendar");
-    
+    String input = scan.nextLine();
+    if(input.equals("1")){
+       addAppointment(int day, Appointment appt );
+    }
+    if(input.equals("2")){
+        delAppointment(int day, String name)
+    }
+    if(input.equals("3")){
+      
+    }
   }
   
 }
